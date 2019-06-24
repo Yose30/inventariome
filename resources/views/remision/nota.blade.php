066f3f3e59cc5d09c04d39e5922c8a6ec368d04a
Yose30 <yos_23@live.com.mx>
2019-06-24 12:52:34 -0500

    nuevo

--- resources/views/remision/nota.blade.php
+++ resources/views/remision/nota.blade.php
@@ -10,11 +10,11 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <!-- Styles -->
         <style>
-            @import url('https://fonts.googleapis.com/css?family=Montserrat&display=swap');
+            @import url('https://fonts.googleapis.com/css?family=Montserrat&display=swap')
             html, body {
                 background-color: #fff;
                 color: #000000;
-                font-family: 'Montserrat', sans-serif;
+                'Nunito', sans-serif;
                 font-weight: 200;
                 height: 100vh;
                 margin: 0;
@@ -88,13 +88,8 @@
                 <div align="center">
                 <table>
                     <tr>
-<<<<<<< HEAD
-                        <!-- <th><img src="{{ asset('img/Globo.png')}}" height="100" width="100" align="right"></th> -->
-                        <th><h1 align="center">Omega Book Company S. A. de C. V.</h1></th>
-=======
                         <th><img src="{{ asset('img/Globo.png')}}" height="100" width="100" align="right" ></th>
                         <th><h1 >Omega Book Company S. A. de C. V.</h1></th>
->>>>>>> 5cac92a5f7ce1b389b2090daa4b3fa7ef0bbe4c9
                         <th></th> 
                         <th></th>
                     </tr>
@@ -129,12 +124,6 @@
                         <th style="width:34%">Dirección:</th>
                     </tr>
                     <tr>
-<<<<<<< HEAD
-                        <td></td>
-                        <td><p>{{ $remision->cliente->condiciones_pago }}</p>
-                        </td>
-                        <td>  <p>{{ $remision->cliente->direccion }}</p></td>
-=======
                         <td rowspan="3">{{ $remision->cliente->name }}</td>
                         <td>
                             <p>{{ $remision->cliente->condiciones_pago }}</p>
@@ -142,7 +131,6 @@
                         <td rowspan="3">  
                             <p>{{ $remision->cliente->direccion }}</p>
                         </td>
->>>>>>> 5cac92a5f7ce1b389b2090daa4b3fa7ef0bbe4c9
                     </tr>
 
                     <tr>
@@ -206,20 +194,14 @@
                     </tr>
                     <tr>
                         <td colspan="3">Nuve millones trecientos sesenta y cinco mil docientos cincuenta y cuatro pesos mx</td>
+                               
+                    </tr>
+                    <tr>
+                         <td colspan="5"><p >Av. del Taller # 460, Col. Jardín Balbuena C. P. 15900 <br/>Del. Venustiano Carranza, Ciudad de México.<br/>Tel: 55-5803-64-15        mail: tere.omega1@hotmail.com</p></td> 
                     </tr>
                 </table>
-                <table>
-                <tr>
-                <td colspan="5">
-                    <p >Av. del Taller # 460, Col. Jardín Balbuena C. P. 15900 <br/>Del. Venustiano Carranza, Ciudad de México.<br/>Tel: 55-5803-64-15        mail: tere.omega1@hotmail.com</p>
-                    <p ></p>
-                    <p></p>
-                </td>        
-                </tr>
-
-                </table>
-                <br>
-                   
+              
+               
                 </div>
                
             </main>
