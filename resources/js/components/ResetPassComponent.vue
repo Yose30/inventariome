10c97639d2ae55cfe482e8b2e7300d7b83c9ec3f
Yose30 <yos_23@live.com.mx>
2019-07-24 17:30:11 -0500

    final

---
+++ resources/js/components/ResetPassComponent.vue
@@ -0,0 +1,38 @@
+<template>
+    <div> 
+        <b-form @submit.prevent="onUpdate">
+            <b-row class="my-1">
+                <b-col sm="3">
+                    <label>Contraseña</label>
+                </b-col>
+                <b-col sm="9">
+                    <b-form-input type="password"></b-form-input>
+                </b-col>
+            </b-row>
+            <b-row class="my-1">
+                <b-col sm="3">
+                    <label>Confirmar contraseña</label>
+                </b-col>
+                <b-col sm="9">
+                    <b-form-input type="password"></b-form-input>
+                </b-col>
+            </b-row>
+        </b-form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+               
+            }
+        },
+        
+        methods: {
+            update(){
+
+            }
+        }
+    }
+</script>
