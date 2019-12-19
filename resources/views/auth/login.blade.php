cf2c26e83489ddebc5c632595c4579f4d1e3980d
Yose30 <yos_23@live.com.mx>
2019-12-19 12:43:36 -0600

    Actualización

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container" style="margin-top: 100px;">
+<div class="container" style="margin-top: 150px;">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="card text-white bg-dark">
                 <div class="card-header text-center">{{ __('Iniciar sesión') }}</div>
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
@@ -13,7 +13,7 @@
                             <label 
                                 for="email" 
                                 class="col-md-4 col-form-label text-md-right">
-                                {{ __('Nombre de usuario') }}
+                                {{ __('Usuario') }}
                             </label>
                             <div class="col-md-6">
                                 <input 
@@ -24,7 +24,6 @@
                                     value="{{ old('user-name') }}" 
                                     required autocomplete="user_name" 
                                     autofocus>
-
                                 @error('user_name')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -52,16 +51,10 @@
                                 @enderror
                             </div>
                         </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Acceder') }}
-                                </button>
-                                <!-- <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('¿Olvidaste tu contraseña?') }}
-                                </a> -->
-                            </div>
+                        <div class="form-group text-center">
+                            <button type="submit" class="btn btn-light">
+                                {{ __('Acceder') }}
+                            </button>
                         </div>
                     </form>
                 </div>
--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 <div class="container">
-    <a class="navbar-brand" style="color:white;" href="#">Inventario ME</a>
+    <a class="navbar-brand" style="color:white;" href="#"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
